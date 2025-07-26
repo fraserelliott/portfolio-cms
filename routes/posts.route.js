ae1d81ce9e7a69ef24facce18e41be2323b83341
Fraser Elliott <fraserelliott@hotmail.com>
2025-07-26 21:15:27 +0100

    fixed filtering function for tags in posts.route.js get route for /api/posts

--- routes/posts.route.js
+++ routes/posts.route.js
@@ -43,7 +43,12 @@ router.get("/", async (req, res) => {
         let where = {};
         if (req.query.featured)
             where.featured = (req.query.featured === "true");
-        let tagWhere = {};
+        const include = {
+            model: Tag,
+            as: 'tags',
+            attributes: ['name'],
+            through: { attributes: [] }, // hide join table IDs
+        }
         const tags = req.query.tags;
         if (tags) {
             // allow either &tags=1&tags=2 or &tags=1,2 to build the tag filter list
@@ -51,16 +56,10 @@ router.get("/", async (req, res) => {
                 ? req.query.tags.map(Number)
                 : req.query.tags.split(',').map(Number);
 
-            tagWhere.id = { [Op.in]: tagIds };
+            include.where = { id: { [Op.in]: tagIds } };
         }
         const posts = await Post.findAll({
-            include: {
-                model: Tag,
-                as: 'tags',
-                where: tagWhere,
-                attributes: ['name'],
-                through: { attributes: [] }, // hide join table IDs
-            },
+            include,
             where
         });
         res.json(posts);
